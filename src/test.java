73404773d6f4f547ca00096f4954c2a2a0c2711c
JingGai0524 <1530730178@qq.com>
2022-10-18 19:25:48 +0800

    first commit

---
+++ src/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        char[] a = new char[]{'a', 's' , 'f'};
+        String ss = String.valueOf(a, 0, 0);
+        System.out.printf(ss);
+        System.out.println(ss.length());
+        String sss = null;
+    }
+}
